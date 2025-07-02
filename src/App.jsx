cb17eea4424df73a899db426b2810485b9384120
Diksha <diksharma.ds100@gmail.com>
2025-07-03 01:31:04 +0530

    Initial commit of my project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+// function App() {
+//   return (
+//     <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl font-bold">
+//       SparkScope Frontend!
+//     </div>
+//   );
+// }
+// src/App.jsx
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl font-bold">
+      SparkScope Frontend!
+    </div>
+  );
+}
+
+export default App;
